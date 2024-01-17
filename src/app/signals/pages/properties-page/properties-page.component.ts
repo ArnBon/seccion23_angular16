bffec95d3ec9724528ffc2f0d8b2ce1bdcc96cf5
ArnBon <bonillaarnaldo@gmail.com>
2024-01-16 20:03:57 -0400

    clase 332 mutaciones actualizar parte del estado

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
@@ -6,8 +7,61 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
-  onFieldUpdated(field: string, value: string){
-    console.log({field, value})
+
+  public user = signal<User>( {
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg'
+  } );
+
+  public fullName = computed( () => `${ this.user().first_name } ${ this.user().last_name }` );
+
+ onFieldUpdated( field: keyof User, value: string ) {
+
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value,
+    // });
+
+    // this.user.update( current => ({
+    //   ...current,
+    //   [field]: value
+    // })) nos da el mismo problema que el de arriba
+
+    this.user.update( current => {
+/***esta mutando el current y lo esta regresando */
+      switch ( field ) {
+
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'avatar':
+          current.avatar = value;
+          break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+
+        case 'id':
+          current.id = Number( value ); // va a ser = al valor numerico del value
+        break;
+      }
+
+
+      return current;
+    } );
+
+
+
+
   }
 
 }
